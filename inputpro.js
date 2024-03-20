8d9473256e9efd2954843ecadc0f1b6cb14a88be
vishu017 <140785990+vishu017@users.noreply.github.com>
2024-03-20 13:22:30 +0530

    first commit

--- inputpro.js
+++
@@ -1,6 +0,0 @@
-const prompt = require('prompt-sync')();
-
-var fname = prompt("enter your first name");
-var lname =prompt("enter your last name");
-
-console.log("your full name is",fname,lname)
--- tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-let avg =ans3.reduce((acc,curr) => acc+curr,0)
-// console.log(avg)
-// console.log(avg/ans3.length) 
