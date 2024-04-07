6daa51ba272712c8c70e255b9739e3626b021a75
vishu017 <140785990+vishu017@users.noreply.github.com>
2024-04-07 14:51:09 +0530

    first commit

---
+++ index5.js
@@ -0,0 +1,16 @@
+
+//     let n = 6
+//     let str = "vishal"
+//     for(let i=0; i<=str.length; i++){
+//     if(num % 2 == 0){
+//         i += 1
+//         console.log(str);
+//     }  
+// }
+var N = 6;
+var str = "ankudh";
+
+for (var i = 1; i <= N; i+=2) {
+   
+    console.log(str[i]);
+}
